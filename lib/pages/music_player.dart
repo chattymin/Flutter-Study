475d98f4b064ec454e88b62d66581d35c910996f
Dongmin <pdm001125@naver.com>
2026-04-16 20:43:17 +0900

    feat top contents

--- lib/pages/music_player.dart
+++ lib/pages/music_player.dart
@@ -2,13 +2,13 @@ import 'package:flutter/material.dart';
 
 class MusicPlayer extends StatelessWidget {
   const MusicPlayer({super.key});
-  final beginColor = const Color.fromARGB(255, 177, 166, 99);
-  final endColor = const Color.fromARGB(255, 98, 89, 54);
+  static const beginColor = Color.fromARGB(255, 177, 166, 99);
+  static const endColor = Color.fromARGB(255, 98, 89, 54);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(
+      decoration: const BoxDecoration(
         gradient: LinearGradient(
           begin: Alignment.topCenter,
           end: Alignment.bottomCenter,
@@ -17,9 +17,54 @@ class MusicPlayer extends StatelessWidget {
       ),
       child: Scaffold(
         backgroundColor: Colors.transparent,
-        body: const Column(children: [
-            
-          ],
+        body: const SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 24),
+            child: Column(
+              children: [
+                /// top content
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(
+                      onPressed: null,
+                      icon: Icon(
+                        Icons.keyboard_arrow_down,
+                        color: Colors.white,
+                        size: 32,
+                      ),
+                    ),
+
+                    Text(
+                      "새소년",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+
+                    IconButton(
+                      onPressed: null,
+                      icon: Icon(
+                        Icons.more_horiz,
+                        color: Colors.white,
+                        size: 32,
+                      ),
+                    ),
+                  ],
+                ),
+
+                // album image
+
+                // album info
+
+                // indicator
+
+                // control buttons
+              ],
+            ),
+          ),
         ),
       ),
     );
